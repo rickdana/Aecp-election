4286a510f13cd623d025085bcedfbc21407fb975
root <christianlouche@yahoo.fr>
2016-09-17 10:42:52 +0200

    Initial Commit. Mise en place du projet

---
+++ src/test/java/fr/aecp/election/AecpElectionApiApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.aecp.election;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AecpElectionApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
